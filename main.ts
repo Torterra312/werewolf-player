1f5e29c171fcb74d656888d1682dcdbcb10971ca
Torterra312 <97889941+Torterra312@users.noreply.github.com>
2022-01-31 15:18:32 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == Player) {
+        basic.showString("Werewolf")
+    } else {
+        basic.showString("Town")
+    }
+})
+let Player = 0
 radio.setGroup(173)
+Player = 1
